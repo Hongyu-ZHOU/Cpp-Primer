ac989ff16b079a2312096deb0ef09ecd223e07ea
Hongyu-ZHOU <62939586+Hongyu-ZHOU@users.noreply.github.com>
2020-05-25 16:15:30 +0800

    Create Ex.cpp

---
+++ Ch3/Ex.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include <cstring>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{
+
+	//string word;
+	//while (getline(cin , word))
+	//	cout << word << endl;
+
+	//vector<int> vec = { 1,2,3,4,5,6,7,8,9 };
+	//for (auto & i : vec)
+	//	i *= i;
+	//for (auto i : vec)
+	//	cout << i << endl;
+
+	//int arr[10];
+	//for (size_t i = 0; i < 10; ++i)
+	//	arr[i] = i;
+	//for (size_t i : arr) cout << i <<  endl;
+
+	////Ex3_39
+	//// use C-Style character strings.
+	//const char cs1[] = "Wangyue";
+	//const char cs2[] = "Pezy";
+	//auto result = strcmp(cs1, cs2);
+	//if (result == 0)
+	//	cout << "same string." << endl;
+	//else if (result < 0)
+	//	cout << "Wangyue < Pezy" << endl;
+	//else
+	//	cout << "Wangyue > Pezy" << endl;
+
+	////Ex3_42
+	//vector<int> v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	//int arr[10];
+	//for (auto i : v)
+	//	arr[i] = v[i];
+	//for (auto i : arr) 
+	//	cout << i <<  endl;
+
+
+
+	return 0;
+};
